4c70c5cd5ab84dd6a4159c489c3918d68bdfe4b4
adrinetor86 <adrinetor81@gmail.com>
2026-02-19 13:22:01 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace McvCoreUtilidades.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
